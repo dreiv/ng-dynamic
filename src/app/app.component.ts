2e9281111c1b80c11cd87e8385b6b70d55870592
Andrei Voicu <Andrei-Florian.Voicu@haufe-lexware.com>
2017-02-09 16:49:16 +0200

    really simple implementation, works but it is not enough.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ViewContainerRef, Compiler, OnInit, NgModule } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    template: `
+    <h1>Dynamic template:</h1>
+    <div #container></div>`
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+    @ViewChild('container', {read: ViewContainerRef}) container: ViewContainerRef;
+
+    private template = `<p>This is compiled injected content: {{1 + 1}}<p>`;
+
+    constructor(private compiler: Compiler) {
+    }
+
+    ngOnInit() {
+        this.addComponent(this.template);
+    }
+
+    private addComponent(template: string) {
+        @Component({template: template})
+        class TemplateComponent {
+        }
+
+        @NgModule({declarations: [TemplateComponent]})
+        class TemplateModule {
+        }
+
+        const mod = this.compiler.compileModuleAndAllComponentsSync(TemplateModule);
+        const factory = mod.componentFactories.find((comp) =>
+            comp.componentType === TemplateComponent
+        );
+        const component = this.container.createComponent(factory);
+    }
 }
